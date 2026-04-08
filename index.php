32b6056055c5bbdc2bb698714362463358cb04e9
RyonTanka <ryontanka@gmail.com>
2026-04-08 21:19:29 +0700

    2532005 carvin

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+
+    </style>
+    <div>
+        <img src="http://10.11.11.78/aplikasi_estafet/assets/Logo_uib.png" alt="Logo_uib.png"> 
+        <label for="NPM"> NPM: </label><br>
+        <input type="text" id="NPM" name="NPM" value="Masukkan NPM"><br>
+        <button type="submit">Submit</button>  <!-- Input login oleh Ryontankawijaya_2532011 -->
+
+    </div>
+    <div>
+        <label for="PASSWORD"> PASSWORD: </label><br>
+        <input type="text" password="PASSOWRD" value="Masukan Password"><br>
+        <button type="submit">Submit</button> <!-- Input login oleh DivaNajwaMeitara_2532027 -->
+    </div>
+
+    <div>
+            <img src="http://10.11.11.78/aplikasi_estafet/assets/Logo_uib.png" alt="Logo_uib.png"> 
+            <br>
+            
+            <label>NPM:</label><br>
+            <input type="text" name="NPM" placeholder="Masukkan NPM" required><br>
+
+            <label>NAMA:</label><br>
+            <input type="text" name="NAMA" placeholder="Masukkan Nama Lengkap" required><br>
+
+            <label>PRODI:</label><br>
+            <input type="text" name="PRODI" placeholder="Program Studi" required><br>
+
+            <label>IPK:</label><br>
+            <input type="number" step="0.01" name="IPK" placeholder="Contoh: 3.50" required><br>
+
+            <label>PAS PHOTO:</label><br>
+            <input type="file" name="FOTO" required><br>
+
+            <br>
+            <button type="submit" name="submit_mhs">Submit Data</button> 
+            </div>!-- Input login oleh feby wahdin_2532074>
+            
+            
+
+
+    
+
+</body>
+</html>
+
+
