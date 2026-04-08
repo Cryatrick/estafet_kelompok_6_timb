82ed5edfd540e6ef043fd30b4144ba732ed1e5a2
RyonTanka <ryontanka@gmail.com>
2026-04-08 20:10:40 +0700

    DivaNajwaMeitara_2532027

--- index..php
+++ index..php
@@ -13,9 +13,15 @@
         <img src="http://10.11.11.78/aplikasi_estafet/assets/Logo_uib.png" alt="Logo_uib.png"> 
         <label for="NPM"> NPM: </label><br>
         <input type="text" id="NPM" name="NPM" value="Masukkan NPM"><br>
-        <button type="submit">Submit</button>
+        <button type="submit">Submit</button>  <!-- Input login oleh Ryontankawijaya_2532011 -->
 
     </div>
+    <div>
+        <label for="PASSWORD"> PASSWORD: </label><br>
+        <input type="text" password="PASSOWRD" value="Masukan Password"><br>
+        <button type="submit">Submit</button> <!-- Input login oleh DivaNajwaMeitara_2532027 -->
+    </div>
+
 </body>
 </html>
 
@@ -28,12 +34,7 @@
 
     $conn = mysqli_connect($host,$user,$pass,$db);
     if(!$conn){
-        die("koneksi gagal:". mysqli_connect_error());
+        die("koneksi gagal:". mysqli_connect_error()); //database connect oleh Ryontankawijaya_2532011
     }
-
-
-
-
-
-
+    //cari tahu gimana mau buka di localhost pathnya di C:\Users\Ryon Tan\OneDrive\Documents\GitHub\estafet_kelompok_6_timb
 ?>
